c6f78fa503f614dc9b0efb88eab777e432d29108
Hitsugata <zakie95hinds@gmail.com>
2017-10-06 21:49:41 -0400

    Some basic things needed to connect project to phunserver

---
+++ Assets/Scripts/Managers/PhoManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoManager : Photon.MonoBehaviour {
+void AutoConnect(string gameVersion )
+    {
+        PhotonNetwork.ConnectUsingSettings(gameVersion);
+    }
+
+    public virtual void OnJoinedLobby()
+    {
+        RoomOptions roomops = new RoomOptions() { IsVisible = true, MaxPlayers = 2 };
+        PhotonNetwork.JoinOrCreateRoom("hello", roomops, TypedLobby.Default);
+        Debug.Log("Hello World");
+    }
+
+    void Start()
+    {
+        AutoConnect("test");
+    }
+}
