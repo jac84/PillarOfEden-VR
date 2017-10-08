40c30aaef79a8c940ed45810cbd4e9fc24787100
Hitsugata <zakie95hinds@gmail.com>
2017-10-07 23:20:35 -0400

    The start of G Manager

---
+++ Assets/Scripts/Managers/GamManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamManager : MonoBehaviour {
+    
+    [SerializeField] private PhoManager Network_Manager;
+
+    // Use this for initialization
+    void Start () {
+        Network_Manager.AutoConnect("test");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Managers/PhoManager.cs
+++ Assets/Scripts/Managers/PhoManager.cs
@@ -3,20 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PhoManager : Photon.MonoBehaviour {
-void AutoConnect(string gameVersion )
+    public void AutoConnect(string gameVersion )
     {
         PhotonNetwork.ConnectUsingSettings(gameVersion);
+        Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
     }
-
-    public virtual void OnJoinedLobby()
+    public void RoomSetup()
     {
         RoomOptions roomops = new RoomOptions() { IsVisible = true, MaxPlayers = 2 };
         PhotonNetwork.JoinOrCreateRoom("hello", roomops, TypedLobby.Default);
         Debug.Log("Hello World");
+
+    }
+
+    //Seems redundant but just in case we want to do other other things when the clinet
+    //joins lobby.
+    public virtual void OnJoinedLobby()
+    {
+        RoomSetup();
+    }
+
+    public virtual void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), 0);
     }
 
     void Start()
     {
-        AutoConnect("test");
+
     }
 }
