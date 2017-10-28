4864c713867eb09e826314a502a5eee7148302f9
Hitsugata <zakie95hinds@gmail.com>
2017-10-27 20:33:35 -0400

    Preparing

---
+++ Assets/Scripts/offsetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class offsetter : MonoBehaviour {
+    public float offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x,offset,transform.position.z);
+	}
+}
